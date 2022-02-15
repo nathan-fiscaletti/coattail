38cf3e4790217fb8897e0092246d4bcf6e5a4bc7
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2022-02-15 23:33:43 +0000

    more changes

--- client_test.js
+++ client_test.js
@@ -1,11 +1,11 @@
 const Client = require(`./lib/client`);
 const Peer = require(`./lib/peer`);
 const Operations = require(`./lib/protocol`);
-const { connect } = require(`./lib/data/connection`);
+const { db } = require(`./lib/data/connection`);
 
-const connection = connect();
-Peer.load(connection, '89476422-4f6f-4b8d-8cbf-35d0b9893b0f').then(peer => {
-    connection.destroy();
+const database = db();
+Peer.load(database, '89476422-4f6f-4b8d-8cbf-35d0b9893b0f').then(peer => {
+    database.destroy();
 
     const client = Client.connect(peer);
     client.on('ready', () => {
--- test.js
+++ test.js
@@ -1,6 +1,2 @@
-const ipRangeCheck  = require(`ip-range-check`);
-
-const range = '0.0.0.0/0';
-const ip = '255.255.255.255';
-
-console.log(ipRangeCheck(ip, range));
+const a = 'ipv4://1.2.3.4/0';
+console.log(a.split('://'));
