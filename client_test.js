8222ac7c10a841386c302752896c55a083587efc
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2022-02-22 21:39:16 +0000

    updates

--- client_test.js
+++ client_test.js
@@ -1,15 +1,19 @@
-const Client = require(`./lib/client`);
 const Peer = require(`./lib/peer`);
-const Operations = require(`./lib/protocol`);
-const { db } = require(`./lib/data/connection`);
 
-const database = db();
-Peer.load({id: '9027e199-79d4-4857-a300-be3790c0e35b'}).then(peer => {
-    database.destroy();
-
-    const client = Client.connect(peer);
-    client.on('ready', () => {
-        client.logger.info('client ready');
-        client.disconnect();
-    });
+Peer.load({id: '3d18c12b-fcc2-4d7d-bb35-cef219f2c8d8'}).then(peer => {
+    peer.subscribeTo('example').then(
+        sub => console.log(`sub: ${sub}`),
+        error => console.log(`err: ${error}`)
+    );
+    // peer.performAction({
+    //     name: 'example',
+    //     data: { name: 'nathan' }
+    // }).then(
+    //     res => console.log(res),
+    //     errors => {
+    //         for (const error of errors) {
+    //             console.log(error);
+    //         }
+    //     }
+    // );
 });
