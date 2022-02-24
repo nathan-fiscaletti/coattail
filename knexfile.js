19c3d898a3addcd10082f28c90724783221a6334
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2022-02-24 00:24:27 +0000

    updates

--- knexfile.js
+++
@@ -1,9 +0,0 @@
-const configuration = require(`./lib/config`).load();
-
-module.exports = {
-    ...configuration.data,
-    migrations: {
-        directory: "./lib/data/migrations",
-        tableName: 'knex_migrations'
-    }
-};
